32bfe52ced76a939d35a13362b3b7cf6e5439557
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2023-02-24 20:57:56 +0100

    Add OTA betas to downloader

--- downloader.py
+++ downloader.py
@@ -4,16 +4,44 @@ from argparse import Namespace
 
 import util
 
+OTA_PLATFORMS = [
+    "ios",
+    "watchos",
+    "tvos",
+    "audioos",
+    "accessory",
+    "macos",
+    "recovery",
+]
+
 
 def download_otas(output_path: str, platform: str):
-    subprocess.run(["ipsw", "download", "ota", "--output", output_path, "-y", "--platform", platform, "--resume-all"])
-    # TODO: we want beta releases too
+    ipsw_ota_download_command = [
+        "ipsw",
+        "download",
+        "ota",
+        "--output",
+        output_path,
+        "-y",
+        "--platform",
+        platform,
+        "--resume-all",
+    ]
+    ipsw_ota_beta_download_command = ipsw_ota_download_command.copy()
+    ipsw_ota_beta_download_command.append("--beta")
+    subprocess.run(ipsw_ota_download_command)
+    subprocess.run(ipsw_ota_beta_download_command)
 
 
 def parse_args() -> Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_path', dest='output_path', required=True, type=util.directory,
-                        help='path to the output directory where the extracted symbols are placed')
+    parser.add_argument(
+        "--output_path",
+        dest="output_path",
+        required=True,
+        type=util.directory,
+        help="path to the output directory where the extracted symbols are placed",
+    )
     return parser.parse_args()
 
 
@@ -25,9 +53,9 @@ def validate_shell_deps():
 def main():
     args = parse_args()
     validate_shell_deps()
-    for platform in ["ios", "watchos", "tvos", "audioos", "accessory", "macos", "recovery"]:
+    for platform in OTA_PLATFORMS:
         download_otas(args.output_path, platform)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
