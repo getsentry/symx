cdcf45888fb0f88d134a675be7eafe91dd343bf4
Ashley Camba <ashwoods@gmail.com>
2023-05-04 22:31:35 +0200

    Add credentials (#5)
    * Add test python access

---
+++ test_access.py
@@ -0,0 +1,21 @@
+import os
+from google.cloud import storage
+from google.auth import default
+
+# Set the GCP bucket name
+# bucket_name = os.environ['GCP_BUCKET_NAME']
+# metadata_path = os.environ['METADATA']
+bucket_name = 'symbol-collector-dev'
+metadata_path = 'metadata.json'
+
+credentials = default()[0]
+storage_client = storage.Client(credentials=credentials)
+
+# Get the GCP bucket object
+bucket = storage_client.bucket(bucket_name)
+
+blob = bucket.blob(metadata_path)
+if blob.exists():
+    print(f'The file {metadata_path} exists in the GCP bucket.')
+else:
+    print(f'The file {metadata_path} does not exist in the GCP bucket.')
