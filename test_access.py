e29c28eee572513ec25439d7e0639f1e63655ec5
Ashley Camba Garrido <ashwoods@gmail.com>
2023-05-05 21:57:24 +0200

    Another test

--- test_access.py
+++ test_access.py
@@ -28,4 +28,7 @@ if __name__ == "__main__":
     # Get the GCP bucket object
     bucket = storage_client.bucket(BUCKET_NAME)
     download_meta_from_gcs(bucket)
+
+    storage_client = storage.Client(project=PROJECT_ID)
+    bucket = storage_client.bucket(BUCKET_NAME)
     download_meta_from_gcs(bucket)
