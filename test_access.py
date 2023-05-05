3f1a2e641d56eda241381a2e2606393ccce7cefd
Ashley Camba Garrido <ashwoods@gmail.com>
2023-05-05 21:49:01 +0200

    Test passing bucket

--- test_access.py
+++ test_access.py
@@ -6,23 +6,25 @@ BUCKET_NAME = os.environ["BUCKET_NAME"]
 PROJECT_ID = os.environ.get("PROJECT", None)
 ARTIFACTS_META_JSON = 'metadata.json'
 
-storage_client = storage.Client(project=PROJECT_ID)
 
-# Get the GCP bucket object
-bucket = storage_client.bucket(BUCKET_NAME)
 
-def load_meta_from_gcs(storage_client: storage.Client) -> {}:
-    print(f"Loading meta-data from {BUCKET_NAME}/{ARTIFACTS_META_JSON}")
-    result = {}
-    bucket = storage_client.get_bucket(BUCKET_NAME)
+def download_meta_from_gcs(bucket) -> None:
+    # Get the GCP bucket object
     blob = bucket.blob(ARTIFACTS_META_JSON)
-    if not blob.exists():
-        return result
 
-    return result
+    # Download the file to a destination
+    if blob.exists():
+        blob.download_to_filename(ARTIFACTS_META_JSON)
+        print(f"Blob {ARTIFACTS_META_JSON} downloaded to {ARTIFACTS_META_JSON}.")
+
 
 if __name__ == "__main__":
     SENTRY_DSN = os.environ.get("SENTRY_DSN", None)
     if SENTRY_DSN:
         sentry_sdk.init(dsn=os.environ["SENTRY_DSN"], traces_sample_rate=1.0)
-    load_meta_from_gcs(storage_client)
+
+    storage_client = storage.Client(project=PROJECT_ID)
+
+    # Get the GCP bucket object
+    bucket = storage_client.bucket(BUCKET_NAME)
+    download_meta_from_gcs(bucket)
