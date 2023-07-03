d3271861004a3a21e1f0687a19358ba24e6b22ad
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2023-07-03 18:11:23 +0200

    Add logging to storage migration.

--- symx/_maintenance.py
+++ symx/_maintenance.py
@@ -1,7 +1,12 @@
+import logging
+
 from google.cloud.storage import Bucket, Blob  # type: ignore
 
 from symx._gcs import GoogleStorage
 from symx._ota import OtaProcessingState
+
+
+logger = logging.getLogger(__name__)
 
 
 def migrate(storage: GoogleStorage) -> None:
@@ -13,6 +18,7 @@ def migrate(storage: GoogleStorage) -> None:
             # reset each meta-data item marked as a `DUPLICATE` back to `MIRRORED`
             # to allow the extraction workflow to pick it up again
             if v.processing_state == OtaProcessingState.BUNDLE_DUPLICATION_DETECTED:
+                logger.info(f"Resetting bundle-id duplicate: {k} to MIRRORED")
                 v.processing_state = OtaProcessingState.MIRRORED
                 v.update_last_run()
                 storage.update_meta_item(k, v)
