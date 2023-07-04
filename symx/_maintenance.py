437b552c06203aac1d58bce8b1dbf94d960b64bd
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2023-07-04 19:41:01 +0200

    wet-run migration

--- symx/_maintenance.py
+++ symx/_maintenance.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+import sentry_sdk
 
 from symx._gcs import GoogleStorage
 
@@ -65,24 +66,26 @@ def migrate(storage: GoogleStorage) -> None:
         for old_bundle_id, ota_key in bundles.items():
             common_path_prefix = f"symbols/{platform}/bundles/"
             new_bundle_id = f"ota_{ota_key}"
+            sentry_sdk.set_tag("ota.bundle.id.old", old_bundle_id)
+            sentry_sdk.set_tag("ota.bundle.id.new", new_bundle_id)
+
             bundle_idx_src_blob_path = f"{common_path_prefix}{old_bundle_id}"
             bundle_idx_dst_blob_path = f"{common_path_prefix}{new_bundle_id}"
             bundle_idx_blob = bucket.blob(bundle_idx_src_blob_path)
             if bundle_idx_blob.exists():
-                # TODO: 3: move that blob
                 logger.info(
-                    f"dry-run: moving {bundle_idx_src_blob_path} to"
-                    f" {bundle_idx_dst_blob_path}"
+                    f"moving {bundle_idx_src_blob_path} to {bundle_idx_dst_blob_path}"
                 )
                 bundle_idx = json.loads(bundle_idx_blob.download_as_string())
 
-                # TODO: 2: overwrite the name in the bundle_idx and store in that blob
                 if bundle_idx["name"] != old_bundle_id:
                     logger.error(
                         f"Expected bundle-index name to be equal to {old_bundle_id}"
                     )
 
                 for debug_id in bundle_idx["debug_ids"]:
+                    sentry_sdk.set_tag("ota.bundle.debug.id", debug_id)
+
                     common_back_ref_prefix = (
                         f"symbols/{platform}/{debug_id[0:2]}/{debug_id[2:]}/refs/"
                     )
@@ -90,16 +93,43 @@ def migrate(storage: GoogleStorage) -> None:
                     new_bundle_id_back_ref = f"{common_back_ref_prefix}{new_bundle_id}"
                     back_ref_blob = bucket.blob(old_bundle_id_back_ref)
                     if back_ref_blob.exists():
-                        # TODO 1: move the back-refs
                         logger.info(
-                            f"dry-run: moving {old_bundle_id_back_ref} to"
+                            f"moving back-ref {old_bundle_id_back_ref} to"
                             f" {new_bundle_id_back_ref}"
                         )
+                        # 1: move all back-refs
+                        try:
+                            bucket.rename_blob(
+                                back_ref_blob,
+                                new_bundle_id_back_ref,
+                                if_generation_match=0,
+                            )
+                        except Exception as e:
+                            sentry_sdk.capture_exception(e)
                     else:
                         logger.error(
                             f"Found debug-id ({debug_id}) in bundle-index"
                             f" {bundle_idx_src_blob_path}, that doesn't back-ref via"
                             f" {old_bundle_id_back_ref}"
                         )
+
+                # 2: overwrite the name in the bundle_idx and store in that blob
+                bundle_idx["name"] = new_bundle_id
+                try:
+                    bundle_idx_blob.upload_from_string(
+                        json.dumps(bundle_idx),
+                        if_generation_match=bundle_idx_blob.generation,
+                    )
+                except Exception as e:
+                    sentry_sdk.capture_exception(e)
+
+                # 3: move the bundle_idx blob
+                try:
+                    bucket.rename_blob(
+                        bundle_idx_blob, bundle_idx_dst_blob_path, if_generation_match=0
+                    )
+                except Exception as e:
+                    sentry_sdk.capture_exception(e)
+
             else:
                 logger.error(f"Can't stat {bundle_idx_src_blob_path}")
