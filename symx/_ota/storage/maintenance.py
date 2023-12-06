8ab59b0fba8cdf63079dfc1bd1050bd85949a1f0
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2023-12-06 20:46:03 +0100

    ota storage maintenance checker

--- symx/_ota/storage/maintenance.py
+++ symx/_ota/storage/maintenance.py
@@ -1,97 +1,87 @@
-import json
 import logging
 
-import sentry_sdk
-
+from symx._common import ArtifactProcessingState
 from symx._ota.storage.gcs import OtaGcsStorage
 
 logger = logging.getLogger(__name__)
 
-bundle_id_map = {
-    "watchos": {
-        "10.0_21R5275t_arm64_32": "c40f9ffd9b9c2e0107ecf69a6980d2ff0d2095aa_beta",
-    },
-}
+otas_to_reset = [
+    "76ca77392a62eb19239ae33b5266b9bb0aabc831",
+    "d57611b8bf0b6f81ec62ff17a5fa5e4e511c2d3e_beta",
+    "827288e19fa2a72d627e4cd96737a4508a9f00bb_beta",
+    "d420a93df15c741b96d88a42840eee21501c97db_beta",
+    "4889c7dcc169f27de39244779df87e6d30c81270_beta",
+    "e63a8ac7a511065b0725cb9ec49169829ef9d8fa_beta",
+    "462921a62fd1347f49a6e5d4485ec81ffe38244d_beta",
+    "0fc25a287a1b93657889cef36da5c7d65291974a_beta",
+    "6683e8ec7572a62a5c01206909148bbea1c24754_beta",
+    "dd27270bea850ef8a5f0c45356a38e5b09c7ba8c_beta",
+    "570f685f442946839d658ed8949436a46ed6d275_beta",
+    "66f18981ad2eebbcb8b2146fcd35587196694323_beta",
+    "678c27d58a6a1a1734c4e959109f423cbd3eabfe_beta",
+    "e8a110d935d35b8553f1e31be6438a2613d7c402_beta",
+    "801f75599001dcdcae7553f9febb6e7e8c0ff836_beta",
+    "aaee1d1fcb97388455622901d20360a44fbc4bfc_beta",
+    "4cd2eab9b3135cae6bf0d6d7e4d5df3c231f0e97_beta",
+    "9de5bd6ecf4616f32787ad02d933fb2a3800f83b_beta",
+    "79f2cb9f28b2f74b3b689c0765477a5c3bcd81a4_beta",
+    "64e37db43b50d5bd87412be39c64fd9524dd53f3_beta",
+    "924999380a30a575cd12ce4da40c6b1d8db13140_beta",
+    "44165d42eff7b876653481a6aeab7d50b188995c_beta",
+    "ad7ce93bd1a766f87970930eac543d236829c8c7_beta",
+    "44e465ef8f6500e51ae814ca8b2ec77e4e7f22b0_beta",
+    "4dd4a1cb58cf68fe327703e85f473a3f5cf5f7c5_beta",
+    "2d41271742e50b533f189fbc11fe459eb558cbf9_beta",
+    "a96c7538340d30cb3232446a750e022e556708e3_beta",
+    "ea54619b2cd0e2a45f9f87fc8cec1ece6b66537c_beta",
+    "425b733851c2bccb507de48183efdba82a01816f_beta",
+    "c32bf27124ccb370f4624eb8d7f3ca90c1b0266f_beta",
+    "1b886c09b4f035f1ddefc2e860850e85383270fc",
+    "8ea82b599c5b8655bacf026f0444d082e9290534",
+    "49aa4785a3d4c3784943e86b2840252d15734719",
+    "87d1ad8823763ca7ad7c53adf440c785c8b722b2",
+    "a756bef2c19b684ef3039213ed145c7fe76c4bbc",
+    "d57611b8bf0b6f81ec62ff17a5fa5e4e511c2d3e",
+    "827288e19fa2a72d627e4cd96737a4508a9f00bb",
+    "d420a93df15c741b96d88a42840eee21501c97db",
+    "4889c7dcc169f27de39244779df87e6d30c81270",
+    "e63a8ac7a511065b0725cb9ec49169829ef9d8fa",
+    "462921a62fd1347f49a6e5d4485ec81ffe38244d",
+    "0fc25a287a1b93657889cef36da5c7d65291974a",
+    "6683e8ec7572a62a5c01206909148bbea1c24754",
+    "dd27270bea850ef8a5f0c45356a38e5b09c7ba8c",
+    "570f685f442946839d658ed8949436a46ed6d275",
+    "66f18981ad2eebbcb8b2146fcd35587196694323",
+    "678c27d58a6a1a1734c4e959109f423cbd3eabfe",
+    "e8a110d935d35b8553f1e31be6438a2613d7c402",
+    "801f75599001dcdcae7553f9febb6e7e8c0ff836",
+    "aaee1d1fcb97388455622901d20360a44fbc4bfc",
+    "4cd2eab9b3135cae6bf0d6d7e4d5df3c231f0e97",
+    "9de5bd6ecf4616f32787ad02d933fb2a3800f83b",
+    "79f2cb9f28b2f74b3b689c0765477a5c3bcd81a4",
+    "64e37db43b50d5bd87412be39c64fd9524dd53f3",
+    "924999380a30a575cd12ce4da40c6b1d8db13140",
+    "44165d42eff7b876653481a6aeab7d50b188995c",
+    "ad7ce93bd1a766f87970930eac543d236829c8c7",
+    "44e465ef8f6500e51ae814ca8b2ec77e4e7f22b0",
+    "4dd4a1cb58cf68fe327703e85f473a3f5cf5f7c5",
+    "2d41271742e50b533f189fbc11fe459eb558cbf9",
+    "a96c7538340d30cb3232446a750e022e556708e3",
+    "ea54619b2cd0e2a45f9f87fc8cec1ece6b66537c",
+    "425b733851c2bccb507de48183efdba82a01816f",
+    "c32bf27124ccb370f4624eb8d7f3ca90c1b0266f",
+]
 
 
 def migrate(storage: OtaGcsStorage) -> None:
-    bucket = storage.bucket
-    for platform, bundles in bundle_id_map.items():
-        for old_bundle_id, ota_key in bundles.items():
-            common_path_prefix = f"symbols/{platform}/bundles/"
-            new_bundle_id = f"ota_{ota_key}"
-            sentry_sdk.set_tag("ota.bundle.id.old", old_bundle_id)
-            sentry_sdk.set_tag("ota.bundle.id.new", new_bundle_id)
+    ota_meta = storage.load_meta()
+    if ota_meta is None:
+        logger.error("Could not retrieve meta-data from storage.")
+        return
 
-            bundle_idx_src_blob_path = f"{common_path_prefix}{old_bundle_id}"
-            bundle_idx_dst_blob_path = f"{common_path_prefix}{new_bundle_id}"
-            bundle_idx_blob = bucket.blob(bundle_idx_src_blob_path)
-            if bundle_idx_blob.exists():
-                logger.info(
-                    f"moving {bundle_idx_src_blob_path} to {bundle_idx_dst_blob_path}"
-                )
-                bundle_idx = json.loads(bundle_idx_blob.download_as_string())
-
-                if bundle_idx["name"] != old_bundle_id:
-                    logger.error(
-                        f"Expected bundle-index name to be equal to {old_bundle_id}"
-                    )
-
-                for debug_id in bundle_idx["debug_ids"]:
-                    sentry_sdk.set_tag("ota.bundle.debug.id", debug_id)
-
-                    common_back_ref_prefix = (
-                        f"symbols/{platform}/{debug_id[0:2]}/{debug_id[2:]}/refs/"
-                    )
-                    old_bundle_id_back_ref = f"{common_back_ref_prefix}{old_bundle_id}"
-                    new_bundle_id_back_ref = f"{common_back_ref_prefix}{new_bundle_id}"
-                    back_ref_blob = bucket.blob(old_bundle_id_back_ref)
-                    if back_ref_blob.exists():
-                        logger.info(
-                            f"found back-ref {old_bundle_id_back_ref}... moving to"
-                            f" {new_bundle_id_back_ref}"
-                        )
-                        # 1: move all back-refs
-                        try:
-                            bucket.rename_blob(
-                                back_ref_blob,
-                                new_bundle_id_back_ref,
-                                if_generation_match=0,
-                            )
-                        except Exception as e:
-                            sentry_sdk.capture_exception(e)
-                    else:
-                        # if we cannot find the old back-ref, the moved one should exist
-                        back_ref_blob = bucket.blob(new_bundle_id_back_ref)
-                        if not back_ref_blob.exists():
-                            # if it doesn't then we'll just create the marker
-                            logger.info(
-                                "couldn't find either new or old back-ref... creating"
-                                f" {new_bundle_id_back_ref}"
-                            )
-                            back_ref_blob.upload_from_string("")
-                        else:
-                            logger.info(
-                                f"back-ref was already move to {new_bundle_id_back_ref}"
-                            )
-
-                # 2: overwrite the name in the bundle_idx and store in that blob
-                bundle_idx["name"] = new_bundle_id
-                try:
-                    bundle_idx_blob.upload_from_string(
-                        json.dumps(bundle_idx),
-                        if_generation_match=bundle_idx_blob.generation,
-                    )
-                except Exception as e:
-                    sentry_sdk.capture_exception(e)
-
-                # 3: move the bundle_idx blob
-                try:
-                    bucket.rename_blob(
-                        bundle_idx_blob, bundle_idx_dst_blob_path, if_generation_match=0
-                    )
-                except Exception as e:
-                    sentry_sdk.capture_exception(e)
-
-            else:
-                logger.error(f"Can't stat {bundle_idx_src_blob_path}")
+    for key, ota in ota_meta.items():
+        if key in otas_to_reset:
+            print(f"{key}: {ota}")
+            assert (
+                ota.processing_state == ArtifactProcessingState.SYMBOL_EXTRACTION_FAILED
+            )
