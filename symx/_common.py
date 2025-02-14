20fa0517b0fa212954f28e1b05bada0fd9a3e143
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2025-02-14 13:10:17 +0100

    refactor sim extraction to pathlib

--- symx/_common.py
+++ symx/_common.py
@@ -409,13 +409,6 @@ def symsort(output_dir: Path, prefix: str, bundle_id: str, split_dir: Path) -> C
 
 def dyld_split(dsc: Path, output_dir: Path) -> CompletedProcess[bytes]:
     return subprocess.run(
-        [
-            "ipsw",
-            "dyld",
-            "split",
-            str(dsc),
-            "--output",
-            str(output_dir),
-        ],
+        ["ipsw", "dyld", "split", str(dsc), "--output", str(output_dir)],
         capture_output=True,
     )
--- symx/_sim/app/__init__.py
+++ symx/_sim/app/__init__.py
@@ -1,6 +1,4 @@
 import logging
-import os
-import sys
 import tempfile
 from dataclasses import dataclass
 from pathlib import Path
@@ -52,18 +50,16 @@ def extract(
     client: Client = Client(project=uri.username)
     bucket: Bucket = client.bucket(uri.hostname)
 
-    caches_path = os.path.expanduser("~/Library/Developer/CoreSimulator/Caches/dyld")
-    if not os.path.isdir(caches_path):
-        sys.exit(f"{caches_path} does not exist")
+    caches_path = Path("~/Library/Developer/CoreSimulator/Caches/dyld").expanduser()
+    if not caches_path.is_dir():
+        raise RuntimeError(f"{caches_path} does not exist")
 
     for runtime in find_simulator_runtimes(caches_path):
         with tempfile.TemporaryDirectory(prefix="_sentry_dyld_shared_cache_") as output_dir:
-            for filename in os.listdir(runtime.path):
-                if not filename.startswith(_dyld_shared_cache_prefix):
+            for filename in runtime.path.iterdir():
+                if not filename.name.startswith(_dyld_shared_cache_prefix) or filename.suffix in (".map", ".dylddata"):
                     continue
-                if os.path.splitext(filename)[1] in (".map", ".dylddata"):
-                    continue
-                runtime.arch = filename.split(_dyld_shared_cache_prefix)[1]
+                runtime.arch = filename.name.split(_dyld_shared_cache_prefix)[1]
                 logging.info(
                     f"Extracting symbols for macOS {runtime.macos_version}, {runtime.os_name} {runtime.os_version} {runtime.arch}"
                 )
@@ -72,49 +68,47 @@ def extract(
             upload_symbol_binaries(bucket, runtime.os_name, runtime.bundle_id, Path(output_dir))
 
 
-def find_simulator_runtimes(caches_path: str) -> List[SimulatorRuntime]:
+def find_simulator_runtimes(caches_path: Path) -> List[SimulatorRuntime]:
     runtimes: List[SimulatorRuntime] = []
-    for macos_version in os.listdir(caches_path):
-        if macos_version == ".DS_Store":
+    for macos_build_path in caches_path.iterdir():
+        if macos_build_path.name == ".DS_Store":
             continue
-        for sim_runtime_name in os.listdir(os.path.join(caches_path, macos_version)):
-            if not sim_runtime_name.startswith(_simulator_runtime_prefix):
+        for runtime_path in macos_build_path.iterdir():
+            if not runtime_path.name.startswith(_simulator_runtime_prefix):
                 continue
-            splits = sim_runtime_name.split(".")
+            splits = runtime_path.name.split(".")
             build_number = splits[5]
             os_info = splits[4].split("-")
             os_version = ".".join(os_info[1:3])
             os_name = os_info[0].lower()
-            path = Path(caches_path) / macos_version / sim_runtime_name
-            for filename in os.listdir(path):
-                if not filename.startswith(_dyld_shared_cache_prefix):
+            for dsc_file in runtime_path.iterdir():
+                if not dsc_file.name.startswith(_dyld_shared_cache_prefix):
                     continue
-                arch = filename.split(_dyld_shared_cache_prefix)[1]
+                arch = dsc_file.name.split(_dyld_shared_cache_prefix)[1]
                 runtimes.append(
                     SimulatorRuntime(
                         arch=arch,
                         build_number=build_number,
-                        macos_version=macos_version,
+                        macos_version=macos_build_path.name,
                         os_name=os_name,
                         os_version=os_version,
-                        path=path,
+                        path=runtime_path,
                     )
                 )
+                # TODO: like in ISPW and OTA, we could try to handle multiple DSCs here instead of breaking after the
+                #   first architecture we found. This was taken over from the implementation in the old uploader.
                 break
     return runtimes
 
 
 def extract_system_symbols(runtime: SimulatorRuntime, output_dir: Path) -> None:
-    for filename in os.listdir(runtime.path):
-        if not filename.startswith(_dyld_shared_cache_prefix):
-            continue
-        if os.path.splitext(filename)[1] in (".map", ".dylddata"):
+    for dsc_file in runtime.path.iterdir():
+        if not dsc_file.name.startswith(_dyld_shared_cache_prefix) or dsc_file.suffix in (".map", ".dylddata"):
             continue
         with tempfile.TemporaryDirectory(prefix="_sentry_dyld_output") as dsc_out_dir:
-            full_path = runtime.path / filename
-            split_result = dyld_split(full_path, Path(dsc_out_dir))
+            split_result = dyld_split(dsc_file, Path(dsc_out_dir))
             if split_result.returncode != 0:
-                raise RuntimeError(f"Failed to split dyld shared cache {full_path}")
+                raise RuntimeError(f"Failed to split dyld shared cache {dsc_file}")
 
             symsort_result = symsort(output_dir, runtime.os_name, runtime.bundle_id, Path(dsc_out_dir))
             if symsort_result.returncode == 0:
