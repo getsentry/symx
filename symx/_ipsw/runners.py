1aa44a88d0e80e8b199f6e2446cc324e573b91f7
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2025-06-13 16:36:51 +0200

    migration of failed 26beta images - verify

--- symx/_ipsw/runners.py
+++ symx/_ipsw/runners.py
@@ -161,7 +161,7 @@ def migrate(ipsw_storage: IpswGcsStorage) -> None:
             if source.file_name in sources:
                 logger.info(f"\t{source.file_name} ({source.processing_state})")
                 sentry_sdk.set_tag("ipsw.artifact.source", source.file_name)
-                assert artifact.sources[source_idx].processing_state == ArtifactProcessingState.SYMBOL_EXTRACTION_FAILED
-                artifact.sources[source_idx].processing_state = ArtifactProcessingState.MIRRORED
-                artifact.sources[source_idx].update_last_run()
-                ipsw_storage.update_meta_item(artifact)
+                assert artifact.sources[source_idx].processing_state == ArtifactProcessingState.MIRRORED
+                # artifact.sources[source_idx].processing_state = ArtifactProcessingState.MIRRORED
+                # artifact.sources[source_idx].update_last_run()
+                # ipsw_storage.update_meta_item(artifact)
